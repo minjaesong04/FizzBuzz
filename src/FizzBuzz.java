6edf186067ce503a3d703764316b18b8ee0ec556
minjaesong04 <minjae.song@mail.utoronto.ca>
2023-09-11 11:41:10 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,7 +5,12 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        int i = 1;
+        doFizzBuzz(i);
+    }
+
+    private static void doFizzBuzz(int i) {
+        while (i < 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -29,6 +34,9 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+
+            i++;
+
         }
     }
 }
